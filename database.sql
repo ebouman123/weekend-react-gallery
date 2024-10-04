c4bacc5e5d0811db799dbcc643e8b82928cd0a4d
Evan Bouman <evan.bouman@gmail.com>
2024-10-04 11:25:56 -0500

    More CSS and added additional values to the sql

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+DROP TABLE "gallery";
+
 CREATE TABLE "gallery" (
   "id" SERIAL PRIMARY KEY,
   "url" VARCHAR,
@@ -9,9 +11,12 @@ CREATE TABLE "gallery" (
 INSERT INTO "gallery" 
 ("url", "title", "description")
 VALUES
-('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.'),
-('images/goat_stache.png', 'Goat Stache!', 'Photo of a mustachioed goat taken at Glacier National Park.'),
 ('images/cat-stare.jpg', 'Cat Stare', 'Photo of a cat staring at the camera'),
 ('images/curious-cat.jpg', 'Curious Cat', 'Photo of a curious cat'),
-('images/upside-down-cat.jpg', 'Upside-down Cat', 'Photo of a cat hanging upside-down');
+('images/upside-down-cat.jpg', 'Upside-down Cat', 'Photo of a cat hanging upside-down'),
+('images/sun-cat.jpg', 'Cat in the Sun', 'Photo of a cat basking in the morning sun'),
+('images/trail-cat.jpg', 'Cat on the Trail', 'Photo of a cat walking on a wooden trail'),
+('images/sleeping-kitty.jpg', 'Cat sleeping soundly', 'Photo of a cat sleeping with their human'),
+('images/big-ears.jpg', 'Big Ears', 'Photo of a cat with large ears'),
+('images/window-cats.jpg', 'Window Cats', 'Photo of two cats looking out of a window');
   
