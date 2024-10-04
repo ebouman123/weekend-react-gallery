8cbb3e40c52100a9601cabc1f7e633049bc4d23e
Evan Bouman <evan.bouman@gmail.com>
2024-10-04 10:34:13 -0500

    Added sql to database.sql, also created folders for future components

--- database.sql
+++ database.sql
@@ -10,5 +10,8 @@ INSERT INTO "gallery"
 ("url", "title", "description")
 VALUES
 ('images/goat_small.jpg', 'Goat!', 'Photo of a goat taken at Glacier National Park.'),
-('images/goat_stache.png', 'Goat Stache!', 'Photo of a mustachioed goat taken at Glacier National Park.');
+('images/goat_stache.png', 'Goat Stache!', 'Photo of a mustachioed goat taken at Glacier National Park.'),
+('images/cat-stare.jpg', 'Cat Stare', 'Photo of a cat staring at the camera'),
+('images/curious-cat.jpg', 'Curious Cat', 'Photo of a curious cat'),
+('images/upside-down-cat.jpg', 'Upside-down Cat', 'Photo of a cat hanging upside-down');
   
