7a98f4ae5f9f1df0cc654d89d15a59527b28680f
Evan Bouman <evan.bouman@gmail.com>
2024-10-05 09:07:02 -0500

    Added some data validation

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -1,27 +1,35 @@
-import { useState } from "react"
-import axios from "axios"
+import { useState } from "react";
+import axios from "axios";
 
-export default function GalleryForm({getGalleryItems}){
+export default function GalleryForm({ getGalleryItems }) {
+  let [URL, setURL] = useState("");
+  let [title, setTitle] = useState("");
+  let [description, setDescription] = useState("");
 
-    let [URL, setURL] = useState("");
-    let [title, setTitle] = useState("");
-    let [description, setDescription] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    
-        axios.post("/api/gallery", { URL: URL, title: title, description: description })
-          .then((response) => {
-            event.target.reset()
-            getGalleryItems();
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      };
+    if (URL.length > 0 && title.length > 0 && description.length > 0) {
+      axios
+        .post("/api/gallery", {
+          URL: URL,
+          title: title,
+          description: description,
+        })
+        .then((response) => {
+          event.target.reset();
+          getGalleryItems();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }else{
+        alert('Please fill out all inputs')
+    }
+  };
 
-    return (
-<>
+  return (
+    <>
       <h2>Add a Cat!</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="urlInput">URL: </label>
@@ -31,22 +39,22 @@ export default function GalleryForm({getGalleryItems}){
           placeholder="Image URL"
           onChange={(event) => setURL(event.target.value)}
         />
-       <label htmlFor = "titleInput">Title: </label>
-        <input 
-        id ="titleInput"
-        type = "text"
-        placeholder="Give your photo a title"
-        onChange={(event) => setTitle(event.target.value)}
+        <label htmlFor="titleInput">Title: </label>
+        <input
+          id="titleInput"
+          type="text"
+          placeholder="Give your photo a title"
+          onChange={(event) => setTitle(event.target.value)}
         />
-        <label htmlFor = "descriptionInput">Description: </label>
-        <input 
-        id ="descriptionInput"
-        type = "text"
-        placeholder="Describe the photo"
-        onChange={(event) => setDescription(event.target.value)}
+        <label htmlFor="descriptionInput">Description: </label>
+        <input
+          id="descriptionInput"
+          type="text"
+          placeholder="Describe the photo"
+          onChange={(event) => setDescription(event.target.value)}
         />
         <button type="submit">Add Cat 🐈</button>
       </form>
     </>
-    )
+  );
 }
